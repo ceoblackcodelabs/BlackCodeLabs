b7d0a27b5b42ec2cf2acc62b2dc52124b04a75b4
Churchillk <churchilkodhiambo@gmail.com>
2026-04-29 23:28:55 +0300

    django qr

--- BlackCodeLabs/settings.py
+++ BlackCodeLabs/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Home',
     # 'SalesPro.apps.SalesproConfig',
-    'QRidentifier.apps.QRIdentifierConfig',
+    'QRIdentifier',
     'Pitchs.apps.PitchsConfig'
 ]
 
